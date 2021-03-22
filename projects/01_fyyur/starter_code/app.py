07a36eb5b1081b1953f5b87a5077fb00333138f9
asripanj <vsaranje@gmail.com>
2021-03-22 14:42:51 -0400

    insert into db setup

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -23,11 +23,18 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
-# TODO: connect to a local postgresql database
+# TODO: connect to a local postgresql database DONE CHECK
 
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
+
+class Shows(db.Model):
+  __tablename__ = 'shows'
+  id = db.Column(db.Integer, primary_key=True)
+  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+  start_time = db.Column(db.DateTime, nullable=False)
 
 class Venue(db.Model):
     __tablename__ = 'Venue'
@@ -38,17 +45,15 @@ class Venue(db.Model):
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120)) #might need to change
+    genres = db.Column(db.ARRAY(db.String(120)))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     website = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean, nullable=False, default = False)
     seeking_description = db.Column(db.String(), nullable=False)
+    shows = db.relationship('Shows', backref='venue', lazy=True) #venue is parent and shows are child
 
-    def __repr__(self): #remove
-      return f'<Venue {self.id} {self.name}>'
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate UPDATE
     
 class Artist(db.Model):
     __tablename__ = 'Artist'
@@ -58,12 +63,13 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120)) #might need to change
+    genres = db.Column(db.ARRAY(db.String(120)))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     website = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean, nullable=False, default = False)
     seeking_description = db.Column(db.String(), nullable=False)
+    shows = db.relationship('Shows', backref='artist', lazy=True) #artist is parent and shows are child
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -231,6 +237,47 @@ def create_venue_form():
 @app.route('/venues/create', methods=['POST'])
 def create_venue_submission():
   # TODO: insert form data as a new Venue record in the db, instead
+  body = {}
+  error = False
+  try:
+    name = request.get_json()['name']
+    city = request.get_json()['city']
+    state = request.get_json()['state']
+    address = request.get_json()['address']
+    phone = request.get_json()['phone']
+    genres = request.get_json()['genres']
+    image_link = request.get_json()['image_link']
+    facebook_link = request.get_json()['facebook_link']
+    website = request.get_json()['website']
+    seeking_talent = request.get_json()['seeking_talent']
+    seeking_description = request.get_json()['seeking_description']
+   
+    venue = Venue(name=name, city=city, state=state, address= address, phone=phone, genres=genres, image_link=image_link, facebook_link=facebook_link, website=website, seeking_talent=seeking_talent, seeking_description=seeking_description)
+    db.session.add(artist)
+    db.session.commit()
+    
+    body['name'] = venue.name
+    body['city'] = venue.city
+    body['state'] = venue.state
+    body['address'] = venue.address
+    body['phone'] = venue.phone
+    body['genres'] = venue.genres
+    body['image_link'] = venue.image_link
+    body['facebook_link'] = venue.facebook_link
+    body['website'] = venue.website
+    body['seeking_talent'] = venue.seeking_talent
+    body['seeking_description'] = venue.seeking_description
+
+  except():
+    db.session.rollback()
+    error =  True
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if error:
+    abort(500)
+  else:
+    return jsonify(body)
   # TODO: modify data to be the data object returned from db insertion
 
   # on successful db insert, flash success
@@ -425,6 +472,46 @@ def create_artist_form():
 def create_artist_submission():
   # called upon submitting the new artist listing form
   # TODO: insert form data as a new Venue record in the db, instead
+  body = {}
+  error = False
+  try:
+    name = request.get_json()['name']
+    city = request.get_json()['city']
+    state = request.get_json()['state']
+    phone = request.get_json()['phone']
+    genres = request.get_json()['genres']
+    image_link = request.get_json()['image_link']
+    facebook_link = request.get_json()['facebook_link']
+    website = request.get_json()['website']
+    seeking_venue = request.get_json()['seeking_venue']
+    seeking_description = request.get_json()['seeking_description']
+   
+    artist = Artist(name=name, city=city, state=state, phone=phone, genres=genres, image_link=image_link, facebook_link=facebook_link, website=website, seeking_venue=seeking_venue, seeking_description=seeking_description)
+    db.session.add(artist)
+    db.session.commit()
+    
+    body['name'] = artist.name
+    body['city'] = artist.city
+    body['state'] = artist.state
+    body['phone'] = artist.phone
+    body['genres'] = artist.genres
+    body['image_link'] = artist.image_link
+    body['facebook_link'] = artist.facebook_link
+    body['website'] = artist.website
+    body['seeking_venue'] = artist.seeking_venue
+    body['seeking_description'] = artist.seeking_description
+
+  except():
+    db.session.rollback()
+    error =  True
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if error:
+    abort(500)
+  else:
+    return jsonify(body)
+
   # TODO: modify data to be the data object returned from db insertion
 
   # on successful db insert, flash success
@@ -490,6 +577,29 @@ def create_shows():
 def create_show_submission():
   # called to create new shows in the db, upon submitting new show listing form
   # TODO: insert form data as a new Show record in the db, instead
+  error = False
+  body = {}
+  try:
+    venue_id = request.get_json()['venue_id']
+    artist_id = request.get_json()['artist_id']
+    start_time = request.get_json()['start_time']
+
+    show = Shows(venue_id=venue_id, artist_id=artist_id, start_time=start_time)
+    db.session.add(show)
+    db.session.commit()
+    body['venue_id'] = show.venue_id
+    body['artist_id'] = show.artist_id
+    body['start_time'] = show.start_time
+  except():
+    db.session.rollback()
+    error = True
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+  if error:
+    abort(500)
+  else:
+    return jsonify(body)
 
   # on successful db insert, flash success
   flash('Show was successfully listed!')
